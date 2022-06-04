8e9b4fd7874501924d715c5e05f9336b8acc7dbe
youngbachhh <dippercode.01@gmail.com>
2022-06-04 22:00:25 +0700

    fix regex register

--- src/main/java/controller/RegisterControl.java
+++ src/main/java/controller/RegisterControl.java
@@ -10,7 +10,6 @@ public class RegisterControl extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
     }
 
     @Override
